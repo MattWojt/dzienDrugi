fcba02ba14345901195f74a7b178a7536547c9b8
MateuszMASZYNA <mr.qrek@gmail.com>
2022-03-09 19:41:51 +0100

    add name request

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,17 @@
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
 public class Main {
+
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Jak masz na imię?  ");
+        String name = scanner.nextLine();
+        System.out.println("Jak masz na nazwisko?  ");
+        String lastname = scanner.nextLine();
+        System.out.println("Ile masz lat?  ");
+        String age = scanner.nextLine();
+        System.out.println("Cześć " +name  +lastname  +age );
+    }
 }
